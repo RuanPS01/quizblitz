876aa6ed2ecc2fd40a85d18053e6b2291caf31df
Ruan Patrick <ruanpatrick.s@hotmail.com>
2026-03-11 01:33:13 -0300

    fix: vite config for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // Ajuste 'base' para o nome do seu repositório no GitHub Pages
+  // Ex: se o repo é 'quizblitz' e fica em usuario.github.io/quizblitz/
+  // Para domínio customizado na raiz, use '/'
+  base: '/quizblitz/',
 })
